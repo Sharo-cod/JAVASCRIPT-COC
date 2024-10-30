2056e99dcd929f1a64eb7af76d24cc7b4e2cf494
Shariq Alam <sharo4192@gmail.com>
2024-10-30 15:04:25 +0500

    some changes

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+console.log("This is shariq alam")
+
+console.log(1 
+    +
+     89) // CODE READABILITY SHOULD BE HIGH              ( DONT DO LIKE THIS )
+
+
+
+     //..............................TYPES OF DATATYPES............................
+    //  NUMBER => 2 TO POWER OF 53
+    //  BigInt
+    //  String => which is written in "" OR ''
+    //  Boolean => True/False
+    //  null = Standalone Value
+    //  undefined =>  
+
+
+     let name = "Shariq Alam" // STRING
+     let age = 124217   // NUMBERS
+     let isLoggedIn = false   //BOLEAN
+     let shariqalam; // undefined
+        
+     console.log(name);
+     console.log(age);
+     console.log(isLoggedIn);
+     
+     console.log(typeof name);
+     console.log(typeof age);
+     console.log(typeof isLoggedIn);
+     console.log(typeof shariqalam);
