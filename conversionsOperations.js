224c7936cdd661eb2747701b5122885b0abb0743
Shariq Alam <sharo4192@gmail.com>
2024-11-01 17:32:01 +0500

    conversions of datatypes

--- conversionsOperations.js
+++ conversionsOperations.js
@@ -1,7 +1,18 @@
-let score = "35";
+// let score = "shariq alam";
 
-console.log(typeof(score));
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+let isLoggedIn = ""               // 1 = true ,  while 0 = false   ,  value like "shariq" = true
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+ 
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
