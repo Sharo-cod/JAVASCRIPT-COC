a71d15b9feead23342d7288e5be878714b1a72a0
Shariq Alam <sharo4192@gmail.com>
2024-11-02 15:39:06 +0500

    datatypes summary

---
+++ datatypes-Summary.js
@@ -0,0 +1,17 @@
+// PRIMITIVE DATATYPES
+// THERE ARE 7 CATEGORIES = string , number , boolean , null , undefined , Symbol , BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const OutsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+
+
+
+// REFERENCE ( NON PRIMITIVE DATATYPES )
+
+// Arrays , Objects , functions 
