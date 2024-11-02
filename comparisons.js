5d2487b605ebd6a102a123637155b74c4eb709fd
Shariq Alam <sharo4192@gmail.com>
2024-11-02 15:28:19 +0500

    Comparisons Complete

---
+++ comparisons.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 > 2);
+// console.log(5 < 9);
+// console.log(5 < 4);
+// console.log(2 == 1);
+// console.log(2 === 2);
+// console.log(2 != 2);
+// console.log(2 != 1);
+
+// ----------------------------------------
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);  // = THIS REASON IS AN EQULAITY CHECK == AND COMPARISONS > <  >=  <= WORK DIFFERENTLY 
+   //NOTES              //   COMPARISONS CONVERT NULL TO A NUMBER, TREATING IT AS 0. 
+                       //   THATS WHY (3) NULL >= 0 IS TRUE AND (1) NULL >0 IS FALSE 
+
+// -----------------------------------------
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// ---------------STRICK CHECKS ------------
+// ===   = STRICK CHECKS
+console.log("2" === 2);
